268a794f6322be07fc84ffe1d4ec0c534af5e21a
Igor Augusto <igoraugustobz@gmail.com>
2024-09-08 08:55:40 -0300

    :tada: init: Commit Inicial

---
+++ palmeiras-api/src/main/java/com/iaugusto/PalmeirasStoreApplication.java
@@ -0,0 +1,13 @@
+package com.iaugusto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmeirasStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalmeirasStoreApplication.class, args);
+	}
+
+}
