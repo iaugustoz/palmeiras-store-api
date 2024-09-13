fe5f7b16a9c8682df044b330c701940ead4bbeba
Igor Augusto <igoraugustobz@gmail.com>
2024-09-12 23:44:34 -0300

    :sparkles: feat: Controller e Repository adicionados

--- palmeiras-api/src/main/java/com/iaugusto/infrastructure/persistence/entities/Product.java
+++ palmeiras-api/src/main/java/com/iaugusto/infrastructure/persistence/entities/Product.java
@@ -1,6 +1,7 @@
 package com.iaugusto.infrastructure.persistence.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Table(name = "produtos")
@@ -15,9 +16,21 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
     private Double price;
+
+    @NotNull
     private String description;
+
+    @NotNull
     private String imageURL;
+
     private Double discount;
+
+    @NotNull
+    private Integer productStock;
 }
