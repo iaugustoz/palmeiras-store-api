18d0654802544fa42e6774fce5cf6e6a87d2f7f3
Igor Augusto <igoraugustobz@gmail.com>
2024-09-14 00:33:56 -0300

    :sparkles: feat: UserRepository criado para a entidade User

---
+++ palmeiras-api/src/test/java/com/iaugusto/infrastructure/controllers/ProductControllerTest.java
@@ -0,0 +1,11 @@
+package com.iaugusto.infrastructure.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductControllerTest {
+
+    @Test
+    @DisplayName("")
+    void method() {}
+}
