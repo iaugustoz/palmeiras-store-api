a1d1d29a09ffe98974516da80a859814c4de0d5e
Igor Augusto <igoraugustobz@gmail.com>
2024-09-14 00:12:27 -0300

    :sparkles: feat: Perfis de usuários adicionados e implementação de UserDetails adicionado a entidade User

---
+++ palmeiras-api/src/main/java/com/iaugusto/core/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.iaugusto.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    private
+}
--- palmeiras-api/src/main/java/com/iaugusto/infrastructure/persistence/entities/User.java
+++ palmeiras-api/src/main/java/com/iaugusto/infrastructure/persistence/entities/User.java
@@ -1,12 +1,20 @@
 package com.iaugusto.infrastructure.persistence.entities;
 
+import com.iaugusto.core.domains.enums.UserRole;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -14,18 +22,64 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String nome;
 
     @Email
-    @NotBlank
+    @NotNull
     private String email;
 
-    @NotBlank
+    @NotNull
     private String password;
-    
+
+    @NotNull
+    private UserRole role; // define o tipo de usuário (ADMIN ou CLIENT)
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) {
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_USER")
+            );
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false; // devo implementar a lógica (por enquanto está o default)
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false; // devo implementar a lógica (por enquanto está o default)
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false; // devo implementar a lógica (por enquanto está o default)
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false; // devo implementar a lógica (por enquanto está o default)
+    }
 }
