b768b770e32e3e4f6aa1cba14eefaaed654c51bf
Igor Augusto <igoraugustobz@gmail.com>
2024-09-14 00:20:29 -0300

    :sparkles: feat: Funcionalidade de obter usuário por username criada

---
+++ palmeiras-api/src/main/java/com/iaugusto/core/services/AuthorizationService.java
@@ -0,0 +1,21 @@
+package com.iaugusto.core.services;
+
+import com.iaugusto.infrastructure.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username);
+    }
+
+}
