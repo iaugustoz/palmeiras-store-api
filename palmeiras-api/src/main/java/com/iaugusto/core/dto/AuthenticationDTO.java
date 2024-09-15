3f43f105593926f82e55c0633170991caf5f2de9
Igor Augusto <igoraugustobz@gmail.com>
2024-09-15 19:00:32 -0300

    :sparkles: feat: Configurações feitas no filterChain() e método para gerar encode/decode das senhas

---
+++ palmeiras-api/src/main/java/com/iaugusto/core/dto/AuthenticationDTO.java
@@ -0,0 +1,4 @@
+package com.iaugusto.core.dto;
+
+public record AuthenticationDTO(String email, String password) {
+}
--- palmeiras-api/src/main/java/com/iaugusto/core/security/SecurityConfig.java
+++ palmeiras-api/src/main/java/com/iaugusto/core/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.iaugusto.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,6 +21,22 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(HttpMethod.POST, "/passarEndpointDepois").hasRole("ADMIN")
+                            .anyRequest().authenticated();
+                })
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
