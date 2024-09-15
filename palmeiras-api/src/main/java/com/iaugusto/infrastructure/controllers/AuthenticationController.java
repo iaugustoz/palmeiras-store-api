206182c1f5b5db00eab1a2e352507224590f3318
Igor Augusto <igoraugustobz@gmail.com>
2024-09-15 18:55:13 -0300

    :sparkles: feat: Controller para tratar de requisições de login criado

---
+++ palmeiras-api/src/main/java/com/iaugusto/infrastructure/controllers/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.iaugusto.infrastructure.controllers;
+
+import com.iaugusto.core.dto.AuthenticationDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+}
